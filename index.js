12342e7f46e72a9b5de7fe5394023e4ca400fe0a
mediatheques <77667283+mediatheques@users.noreply.github.com>
2026-02-10 11:14:43 +0100

    Refactor score handling and update links

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ $(".gagne").on("click", function() {
   } else if (score == 4) {
     $(".scoreFinal").text("Mais en fait vous avez eu 4/6. C'est quand même au-dessus de la moyenne, bien joué.")
   } else if (score == 3) {
-    $(".scoreFinal").text("En fait vous avez eu 3/6. Bel effort tout de même.")
+    $(".scoreFinal").text("Mais en fait vous avez eu 3/6. Vous êtes sur la bonne voie.")
   } else if (score == 2) {
     $(".scoreFinal").text("Mais en fait vous avez eu 2/5. Raspoutine vous aurait mangé tout cru.")
   }
@@ -43,11 +43,13 @@ $(".gagne").on("click", function() {
 $(".restart").on("click", score = 0)
 
 
-var thatone = Math.floor(Math.random() * 6)
-var livres = ["https://mediatheques.sudestavenir.fr/recherche/viewnotice/id_sigb/361795/id_int_bib/10/id_site/35/record_type/1", 
+var thatone = Math.floor(Math.random() * 7)
+var livres = ["https://mediatheques.sudestavenir.fr/recherche/viewnotice/id_sigb/361795/id_int_bib/10/id_site/35/record_type/1",
+              "https://mediatheques.sudestavenir.fr/recherche/viewnotice/id_sigb/288254/id_int_bib/11/id_site/36/record_type/1",
               "https://mediatheques.sudestavenir.fr/recherche/viewnotice/id_sigb/496589/id_int_bib/50/id_site/53/record_type/1", 
               "https://mediatheques.sudestavenir.fr/recherche/viewnotice/id_sigb/383078/id_int_bib/10/id_site/35/record_type/1", 
               "https://mediatheques.sudestavenir.fr/recherche/viewnotice/id_sigb/172107/id_int_bib/10/id_site/35/record_type/1", 
               "https://mediatheques.sudestavenir.fr/recherche/viewnotice/id_sigb/2364092/id_int_bib/10/id_site/35/record_type/1"]
 $(".hasard").attr("href", livres[thatone])
+
 
